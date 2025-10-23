b70ac1ce15fcebf7b709b612fc009114f8f06285
Md Shoaib Md Mukhtar Bagwan <shoaibshaikh643@gmail.com>
2025-10-23 13:33:07 +0530

    remove nginx and php installation from script

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -32,36 +32,7 @@ if [[ $EUID -eq 0 ]]; then
    print_warning "Please create a non-root user and run this script with sudo privileges"
    exit 1
 fi
-
-# Update system
-print_status "Updating system packages..."
-sudo apt update && sudo apt upgrade -y
-
-# Install basic packages
-print_status "Installing basic packages..."
-sudo apt install -y curl wget git unzip software-properties-common apt-transport-https ca-certificates gnupg lsb-release
-
-# Install Nginx
-print_status "Installing Nginx..."
-sudo apt install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-# Install PHP 8.2
-print_status "Installing PHP 8.2 and extensions..."
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt update
-if ! sudo apt install -y php8.2 php8.2-fpm php8.2-mysql php8.2-xml php8.2-gd php8.2-opcache \
-    php8.2-mbstring php8.2-tokenizer php8.2-json php8.2-bcmath php8.2-zip \
-    php8.2-curl php8.2-cli php8.2-intl php8.2-soap php8.2-xmlrpc; then
-    print_warning "PHP 8.2 installation failed, falling back to default PHP version..."
-    sudo apt install -y php php-fpm php-mysql php-xml php-gd php-opcache \
-        php-mbstring php-tokenizer php-json php-bcmath php-zip \
-        php-curl php-cli php-intl
-fi
-
-sudo systemctl start php8.2-fpm
-sudo systemctl enable php8.2-fpm
+ 
 
 # Install MySQL
 print_status "Installing MySQL..."
